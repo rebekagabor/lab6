3a8fa87a0dc2fb21067f312262bba0f6114123e5
cti22a214 <gabor.rebeka@student.uordea.ro>
2023-11-06 20:11:59 +0200

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Employee
+{
+    private String name;
+    private String email;
+    private int hourrate;
+    private int capacity;
+    private int freedays;
+
+    public Employee(String name,String email,int hourrate,int capacity,int freedays)
+    {
+        this.name=name;
+        this.email=email;
+        this.hourrate=hourrate;
+        this.capacity=capacity;
+        this.freedays=freedays;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public int getHourrate()
+    {
+        return hourrate;
+    }
+
+    public int getCapacity()
+    {
+        return capacity;
+    }
+
+    public  int getFreedays()
+    {
+        return freedays;
+    }
+
+    public void setname(String name)
+    {
+        this.name=name;
+    }
+
+    public void setemail(String email)
+    {
+        this.email=email;
+    }
+
+    public void sethourrate(int hourrate)
+    {
+        this.hourrate=hourrate;
+    }
+
+    public void setcapacity(int capacity)
+    {
+        this.capacity=capacity;
+    }
+
+    public void setfreedays(int freedays)
+    {
+        this.freedays=freedays;
+    }
+
+    public int calculateDailyIncome() {
+        return hourrate * capacity;
+    }
+
+    public int calculateMonthlyIncome()
+    {int dailyincome=calculateDailyIncome();
+        return dailyincome*(30-freedays);
+
+    }
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("n=");
+        int n=scanner.nextInt();
+        ArrayList<Employee>employees=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("name:");
+            String name=scanner.next();
+            System.out.println("email:");
+            String email=scanner.next();
+            System.out.println("hourly rate:");
+            int hourrate=scanner.nextInt();
+            System.out.println("capacity work hours per day:");
+            int capacity=scanner.nextInt();
+            System.out.println("free days:");
+            int freedays=scanner.nextInt();
+
+            Employee employee=new Employee (name, email,hourrate,capacity,freedays);
+                    employees.add(employee);
+        }
+        System.out.println("monthly salaries:");
+        for(Employee employee:employees)
+        {
+            System.out.println("name:"+ employee.getName());
+            System.out.println("monthly income:"+employee.calculateMonthlyIncome());
+            System.out.println();
+        }
+    }
+}
